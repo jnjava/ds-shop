3a7310a3ec899e1d9e1338a6c7240fc89505ec0e
jnjava <32747042@qq.com>
2018-03-21 17:37:26 +0800

    搭建骨架

---
+++ ds-shop-data/src/main/java/com/dscomm/shop/data/App.java
@@ -0,0 +1,13 @@
+package com.dscomm.shop.data;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
